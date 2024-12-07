eaf920dfc18136038b9487cd8848a2b1c6e70bfa
Yuguigamer <yuniordavalos@outlook.es>
2024-12-06 21:31:58 -0400

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { PipesComponent } from './pages/pipes/pipes.component';
+import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    AboutComponent,
+    CapitalizadoPipe,
+    PipesComponent,
+    ListaProductosComponent
   ],
   imports: [
     BrowserModule,
